4c88b1e0b6e156d18e2f8b32ce7f932b9c84c114
Clemente <clementem50348@students.laalliance.org>
2017-11-17 17:52:12 +0000

    ok

---
+++ loops.js
@@ -0,0 +1,5 @@
+for (var day = 1; day<= 365; day = day + 1){
+    console.log('I have had my car for ' + day + 'days.')
+    console.log('I have driven ' + (day + 20) + 'miles.')
+    
+}
---
+++ sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(5000,400)
+}
+
+
+function setup() {
+  createCanvas(500,400)
+}
+
+function draw() {
+  background(0)
+  fill(250)
+
+  var x = 40
+
+  for (var i = 0; i < 8; i++) {
+    rect(x, height/10, 40, 40)
+    x += 40
+  }
+
+}
+
+
+  
